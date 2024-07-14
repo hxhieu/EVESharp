776253ab63ebaf542b23b0737ef1411823e1a521
Hugh Hoang <hugh.hoang@gmail.com>
2024-07-14 19:15:11 +1200

    feat: password v2

--- Server/EVESharp.Database.Entity/Account.cs
+++ Server/EVESharp.Database.Entity/Account.cs
@@ -1,37 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace EVESharp.Database.Entity;
 
-[Table("account")]
-[Index("AccountName", Name = "accountName", IsUnique = true)]
-[MySqlCharSet("utf8mb3")]
-[MySqlCollation("utf8mb3_unicode_ci")]
+[Table ("account")]
+[Index ("AccountName", Name = "accountName", IsUnique = true)]
+[MySqlCharSet ("utf8mb3")]
+[MySqlCollation ("utf8mb3_unicode_ci")]
 public partial class Account
 {
     [Key]
-    [Column("accountID", TypeName = "int(10) unsigned")]
+    [Column ("accountID", TypeName = "int(10) unsigned")]
     public uint AccountId { get; set; }
 
-    [Column("accountName")]
-    [StringLength(48)]
+    [Column ("accountName")]
+    [StringLength (48)]
     public string AccountName { get; set; } = null!;
 
-    [Column("password", TypeName = "blob")]
-    public byte[] Password { get; set; } = null!;
+    [Column ("password", TypeName = "blob")]
+    public byte [] Password { get; set; } = null!;
 
-    [Column("role", TypeName = "bigint(20) unsigned")]
+    [Column ("role", TypeName = "bigint(20) unsigned")]
     public ulong Role { get; set; }
 
-    [Column("online")]
+    [Column ("online")]
     public bool Online { get; set; }
 
-    [Column("banned")]
+    [Column ("banned")]
     public bool Banned { get; set; }
 
-    [Column("proxyNodeID", TypeName = "bigint(20)")]
+    [Column ("proxyNodeID", TypeName = "bigint(20)")]
     public long ProxyNodeId { get; set; }
+
+    [Column ("password_v2")]
+    [MaxLength (80)]
+    public string? PasswordV2 { get; set; }
 }
---
+++ Server/EVESharp.Database.Entity/Migrations/20240714071400_AddAccountPasswordV2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EVESharp.Database.Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountPasswordV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "password_v2",
+                table: "account",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: true,
+                collation: "utf8mb3_unicode_ci")
+                .Annotation("MySql:CharSet", "utf8mb3");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "password_v2",
+                table: "account");
+        }
+    }
+}
